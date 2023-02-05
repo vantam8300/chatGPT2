3070dd915222225e840e1ee3c1ac0294f7039cba
vantam8300 <vantam8300@gmail.com>
2023-02-04 21:33:54 -0800

    adding api file

---
+++ client/src/api.js
@@ -0,0 +1,18 @@
+export const fetchResponse =  async(chat) => {
+    try {
+        const response = await fetch('http://localhost:3080', { 
+            method: 'POST',
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                message: chat.map((message)=> message.message).join(" \n ")
+            })
+        })
+
+        const data = await response.json()
+        return data
+    } catch (error) {
+        console.log(error);
+    }
+}
